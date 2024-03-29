88f912596a71308525558378ca7cd04e690ee1ef
pradityahendrik <pradityahendrik@gmail.com>
2024-03-29 15:20:27 +0700

    fix sequelize

--- databases/migrations/20240329081239-create-url-mapping-table.js
+++ databases/migrations/20240329081239-create-url-mapping-table.js
@@ -1,13 +1,9 @@
 'use strict';
 
-const tableOpts = {
-  tableName: 'url_mapping'
-};
-
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, DataTypes) {
-    queryInterface.createTable(tableOpts, {
+    queryInterface.createTable('url_mapping',{
       id: {
         type: DataTypes.UUID,
         defaultValue: DataTypes.UUIDV4,
@@ -15,29 +11,23 @@ module.exports = {
         primaryKey: true
       },
       long_url: {
-          type: DataTypes.TEXT,
-          allowNull: false
+        type: DataTypes.STRING,
       },
       short_code: {
         type: DataTypes.STRING,
-        allowNull: false
       },
       created_at: {
-          type: DataTypes.DATE,
-          allowNull: true
+        type: DataTypes.DATE,
+        allowNull: true
       },
       updated_at: {
-          type: DataTypes.DATE,
-          allowNull: true
-      },
-      deleted_at: {
-          type: DataTypes.DATE,
-          allowNull: true
+        type: DataTypes.DATE,
+        allowNull: true
       }
     })
   },
 
   async down (queryInterface, DataTypes) {
-    queryInterface.dropTable(tableOpts)
+    queryInterface.dropTable('url_mapping')
   }
 };
